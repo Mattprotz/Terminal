8dfa7b9561d19f70f1d6b214ea251890361e5afd
Matthew Proteau <matthew.proteau@gmail.com>
2024-06-26 14:15:39 -0700

    another home update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div className='view-body'>
+    <div className='home-body'>
        <img src={`${process.env.PUBLIC_URL}/Home.gif`} alt="Home" className="home-gif" />
     </div>
   )
