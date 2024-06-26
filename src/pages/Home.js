b6a2eb709ac88264b543a7333fc54b4727632ceb
Matthew Proteau <matthew.proteau@gmail.com>
2024-06-26 15:12:08 -0700

    home page update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 export default function Home() {
+  
+
   return (
     <div className='home-body'>
-      MATTHEW PROTEAU
+      WELCOME USER
+      <br/>
+      please select a page to begin...
        {/* <img src={`${process.env.PUBLIC_URL}/Home.gif`} alt="Home" className="home-gif" /> */}
     </div>
   )
